d581a40e2a4cc16d5ccf693dbcbe1ec6543882a8
Kidus <98009847+kid-us@users.noreply.github.com>
2023-08-25 18:23:42 +0300

    Update Banner.jsx

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -15,7 +15,8 @@ const Banner = ({ fetchUrl }) => {
 
     return (
         <>
-            <header className='banner-container'
+            {/* Large Device Banner */}
+            <header className='banner-container d-none d-md-block'
                 style={{
                     backgroundSize: "cover",
                     backgroundImage: `linear-gradient(to bottom, rgba(1, 1, 1, 0.20), rgba(0, 0, 0, 0.99)), url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path || movie?.poster_path}")`,
@@ -36,6 +37,30 @@ const Banner = ({ fetchUrl }) => {
                     </div>
                 </div>
             </header >
+
+            {/* Small device Banner */}
+            <header className='banner-container d-block d-md-none'
+                style={{
+                    backgroundSize: "cover",
+                    backgroundImage: `linear-gradient(to bottom, rgba(1, 1, 1, 0.20), rgba(0, 0, 0, 0.99)), url("https://image.tmdb.org/t/p/original/${movie?.poster_path || movie?.backdrop_path}")`,
+                    backgroundPosition: "center top",
+                    paddingTop: "50px"
+                }}
+            >
+                <div className="banner-contents px-3 px-lg-5 px-md-5">
+                    <div className="mx-lg-5 px-lg-5">
+                        <div className="banner-title-wrapper">
+                            <p className='banner-title display-5 fw-semibold'>{movie?.title || movie?.name}</p>
+                        </div>
+                        <p className='mt-4'>
+                            <Link to={`/${movie.media_type}/${movie.id}`} className='px-5 btn btn-warning py-1 bi-box-arrow-in-right'></Link>
+                        </p>
+                        <p className='mt-3 fw-semibold text-grey banner-overview'>{movie?.overview}</p>
+
+                    </div>
+                </div>
+            </header >
+
         </>
     )
 }
