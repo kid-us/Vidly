2ee34a1c84fad79b8d3004ac258645f7e9cbda27
kid-us <kidushh29@gmail.com>
2025-04-04 11:32:20 +0300

    Auto focus search input.

--- src/component/Search/Search.jsx
+++ src/component/Search/Search.jsx
@@ -1,101 +1,129 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from '../constant/axios';
-import { Link } from 'react-router-dom';
-import NavBar from '../Navbar/NavBar';
-import Footer from '../Footer/Footer';
-import { allGenres } from '../constant/genres';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "../constant/axios";
+import { Link } from "react-router-dom";
+import NavBar from "../Navbar/NavBar";
+import Footer from "../Footer/Footer";
+import { allGenres } from "../constant/genres";
 
 const imgUrl = "https://image.tmdb.org/t/p/original";
 const Search = () => {
-    const [searchedItem, setSearchedItem] = useState([]);
-    const [hoverIndex, setHoverIndex] = useState();
-    const [hover, setHover] = useState(false);
-    const { media } = useParams();
-    const { query } = useParams();
+  const [searchedItem, setSearchedItem] = useState([]);
+  const [hoverIndex, setHoverIndex] = useState();
+  const [hover, setHover] = useState(false);
+  const { media } = useParams();
+  const { query } = useParams();
 
-    const genreName = (genre) => {
-        const pick = allGenres.filter(g => g.key === genre);
-        return (pick[0].name);
-    }
-    const url = `/search/${media}?query=${query}&api_key=ce65ec21ad9cb3dc7a41a4c1a45629ca`;
+  const genreName = (genre) => {
+    const pick = allGenres.filter((g) => g.key === genre);
+    return pick[0].name;
+  };
+  const url = `/search/${media}?query=${query}&api_key=ce65ec21ad9cb3dc7a41a4c1a45629ca`;
 
-    useEffect(() => {
-        async function fetchQuery() {
-            const searchQuery = await axios.get(url)
-            if (searchQuery.data.results.length !== 0) {
-                const randomSearch = new Set();
-                if (searchQuery.data.results.length > 18) {
-                    while (randomSearch.size < 18) {
-                        const data = searchQuery.data.results[Math.floor(Math.random() * searchQuery.data.results.length)]
-                        randomSearch.add(data);
-                    }
-                    const randomData = Array.from(randomSearch);
-                    setSearchedItem(randomData);
-                } else setSearchedItem(searchQuery.data.results);
-            }
-            return searchQuery;
-        }
-
-        fetchQuery()
-    }, [query]);
-
-    const handleHover = (stat, index) => {
-        setHover(stat);
-        setHoverIndex(index);
+  useEffect(() => {
+    async function fetchQuery() {
+      const searchQuery = await axios.get(url);
+      if (searchQuery.data.results.length !== 0) {
+        const randomSearch = new Set();
+        if (searchQuery.data.results.length > 18) {
+          while (randomSearch.size < 18) {
+            const data =
+              searchQuery.data.results[
+                Math.floor(Math.random() * searchQuery.data.results.length)
+              ];
+            randomSearch.add(data);
+          }
+          const randomData = Array.from(randomSearch);
+          setSearchedItem(randomData);
+        } else setSearchedItem(searchQuery.data.results);
+      }
+      return searchQuery;
     }
 
-    return (
-        <>
-            <NavBar searchFor={"multi"} />
+    fetchQuery();
+  }, [query]);
 
-            <div className="container mt-5 pt-5">
-                <p className="fs-5 mt-5 fw-semibold">Search Resulst for <span className='text-warning'>"{query}"</span></p>
-                <div className="row justify-content-center fw-semibold">
-                    {searchedItem.map((search, index) => (
-                        <div key={index} className="col-lg-2 col-md-4 col-6 my-4">
-                            <div className='movie-container position-relative'>
-                                <Link to={`/${search.media_type}/${search.id}`} onMouseEnter={() => handleHover(true, index)} onMouseLeave={() => handleHover(false, index)}>
+  const handleHover = (stat, index) => {
+    setHover(stat);
+    setHoverIndex(index);
+  };
 
-                                    <img key={search?.title} className='img-fluid rounded vidly-content' src={`${imgUrl}${search?.poster_path || search?.backdrop_path}`} alt={search?.title
-                                    } />
+  return (
+    <>
+      <NavBar searchFor={"multi"} />
 
-                                    {hover &&
-                                        hoverIndex === index
-                                        ?
-                                        <div className="d-sm-none d-md-none d-lg-block">
-                                            <div className=
-                                                {` ${index === 5 || index === 11 || index === 17 ? "movie-modal-left" : "movie-modal-right"} small rounded px-3 py-4 hover-show`}
-                                            >
-                                                <p className='fs-6 text-warning'>{search?.name || search?.title}</p>
-                                                <p className='small'>
-                                                    {
-                                                        search.genre_ids.map(id => (
-                                                            <span className='text-info small me-2' key={id}>{genreName(id)}</span>
-                                                        ))
-                                                    }
-                                                </p>
-                                                <p className="small">
-                                                    <span>{search?.release_date || search?.first_search_date} </span> &nbsp; &nbsp;
-                                                    <span className='bi-star-fill text-warning'> {search.vote_average}</span>
-                                                </p>
-                                                <p className='small text-secondary modal-overview'>{search.overview}</p>
-                                            </div>
-                                        </div>
-                                        :
-                                        ""
-                                    }
-                                </Link>
-                            </div>
-                            <p className='item-name small mt-2 text-secondary fw-semibold'>{search?.name || search?.title}</p>
-                        </div>
-                    ))}
-                </div >
+      <div className="container mt-5 pt-5">
+        <p className="fs-5 mt-5 fw-semibold">
+          Search Resulst for <span className="text-warning">"{query}"</span>
+        </p>
+        <div className="row justify-content-center fw-semibold">
+          {searchedItem.map((search, index) => (
+            <div key={index} className="col-lg-2 col-md-4 col-6 my-4">
+              <div className="movie-container position-relative">
+                <Link
+                  to={`/${search.media_type}/${search.id}`}
+                  onMouseEnter={() => handleHover(true, index)}
+                  onMouseLeave={() => handleHover(false, index)}
+                >
+                  <img
+                    key={search?.title}
+                    className="img-fluid rounded vidly-content"
+                    src={`${imgUrl}${
+                      search?.poster_path || search?.backdrop_path
+                    }`}
+                    alt={search?.title}
+                  />
+
+                  {hover && hoverIndex === index ? (
+                    <div className="d-sm-none d-md-none d-lg-block">
+                      <div
+                        className={` ${
+                          index === 5 || index === 11 || index === 17
+                            ? "movie-modal-left"
+                            : "movie-modal-right"
+                        } small rounded px-3 py-4 hover-show`}
+                      >
+                        <p className="fs-6 text-warning">
+                          {search?.name || search?.title}
+                        </p>
+                        <p className="small">
+                          {search.genre_ids.map((id) => (
+                            <span className="text-info small me-2" key={id}>
+                              {genreName(id)}
+                            </span>
+                          ))}
+                        </p>
+                        <p className="small">
+                          <span>
+                            {search?.release_date || search?.first_search_date}{" "}
+                          </span>{" "}
+                          &nbsp; &nbsp;
+                          <span className="bi-star-fill text-warning">
+                            {" "}
+                            {search.vote_average}
+                          </span>
+                        </p>
+                        <p className="small text-secondary modal-overview">
+                          {search.overview}
+                        </p>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </Link>
+              </div>
+              <p className="item-name small mt-2 text-secondary fw-semibold">
+                {search?.name || search?.title}
+              </p>
             </div>
+          ))}
+        </div>
+      </div>
 
-            <Footer />
-        </>
-    )
-}
+      <Footer />
+    </>
+  );
+};
 
-export default Search
+export default Search;
--- src/component/Search/SearchInput.jsx
+++ src/component/Search/SearchInput.jsx
@@ -1,57 +1,67 @@
-import React, { useState } from 'react'
-import SearchDisplay from './SearchDisplay';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import SearchDisplay from "./SearchDisplay";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const SearchInput = ({ searchFor }) => {
-    const [searchQuery, setSearchQuery] = useState("");
-    const [value, setValue] = useState(0);
-    const [search, setSearch] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [value, setValue] = useState(0);
+  const [search, setSearch] = useState([]);
 
-    const fetchSearch = (value) => {
-        if (value && value !== "") {
-            const url = `https://api.themoviedb.org/3/search/${searchFor}?query=${value}&api_key=ce65ec21ad9cb3dc7a41a4c1a45629ca`;
+  const fetchSearch = (value) => {
+    if (value && value !== "") {
+      const url = `https://api.themoviedb.org/3/search/${searchFor}?query=${value}&api_key=ce65ec21ad9cb3dc7a41a4c1a45629ca`;
 
-            axios.get(url).then(function (response) {
-                if (response.data.results.length !== 0)
-                    setSearch(response.data.results);
-            }).catch(function (error) {
-                console.log(error);
-            });
-        }
+      axios
+        .get(url)
+        .then(function (response) {
+          if (response.data.results.length !== 0)
+            setSearch(response.data.results);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
+  };
 
-    const handleChange = (value) => {
-        setSearchQuery(value);
-        fetchSearch(value);
-        setValue(value.length);
-    }
+  const handleChange = (value) => {
+    setSearchQuery(value);
+    fetchSearch(value);
+    setValue(value.length);
+  };
 
-    return (
-        <>
-            <input type="search" className='search-input px-4 color-black' placeholder='Search Movies and Tv Shows' onChange={e => handleChange(e.target.value)} />
+  return (
+    <>
+      <input
+        type="search"
+        className="search-input px-4 color-black"
+        placeholder="Search Movies and Tv Shows"
+        onChange={(e) => handleChange(e.target.value)}
+        autoFocus
+      />
 
-            <div className="position-relative">
-                {
-                    value !== 0
-                        ?
-                        <div className='search-wrapper'>
+      <div className="position-relative">
+        {value !== 0 ? (
+          <div className="search-wrapper">
+            <SearchDisplay search={search} media={searchFor} value={setValue} />
 
-                            <SearchDisplay search={search} media={searchFor} value={setValue} />
+            <Link
+              to={`/search/${searchFor}/${searchQuery}`}
+              className="text-decoration text-light"
+              onClick={() => setValue(0)}
+            >
+              <p className="text-center bg-warning py-2 rounded mx-5 mt-4 small">
+                View all Search Results &nbsp;&nbsp;
+                <span className="text-dark bi-arrow-up-right-circle-fill"></span>
+              </p>
+            </Link>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </>
+  );
+};
 
-                            <Link to={`/search/${searchFor}/${searchQuery}`} className='text-decoration text-light' onClick={() => setValue(0)}>
-                                <p className='text-center bg-warning py-2 rounded mx-5 mt-4 small'>
-                                    View all Search Results &nbsp;&nbsp;
-                                    <span className='text-dark bi-arrow-up-right-circle-fill'></span>
-                                </p>
-                            </Link>
-                        </div>
-                        :
-                        ""
-                }
-            </div>
-        </>
-    )
-}
-
-export default SearchInput
+export default SearchInput;
